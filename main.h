1837ab5f2904e28ae17c7b72d73bbd4cef529c75
RhodaAdigun <ifeoluwa.adigun2102@gmail.com>
2023-08-22 15:50:10 +0000

    printf project

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 
 #define BUFSIZE 1024
+#define UNUSED(x) (void)(x)
 
 /**
  *
@@ -22,5 +23,15 @@ int _flags(const char *format, int *i);
 int _width(const char *format, int *i, va_list arg);
 int _precision(const char *format, int *i, va_list arg);
 int _size(const char *format, int *i);
+
+int print_char(va_list arg, char buffer[], int flags, int width, int precision, int size);
+int write_char(char c, char buffer[],int flags, int width, int precision, int size);
+int print_percent(va_list arg, char buffer[], int flags, int width, int precision, int size);
+int print_str(va_list arg, char buffer[], int flags, int width, int precision, int size);
+
+int print_int(va_list arg, char buffer[], int flags, int width, int precision, int size);
+long int convert_size(long int num, int size);
+int write_number(int is_negative, int i, char buffer[], int flags, int width, int precision, int size);
+int write_num(int ind, char buffer[],int flags, int width, int prec,int length, char padd, char extra_c);
 
 #endif
--- print_func.c
+++ print_func.c
@@ -4,9 +4,111 @@
  *
  */
 
-int print_char(va_list arg, char buffer[], int i)
+int print_char(va_list arg, char buffer[], int flags, int width, int precision, int size)
 {
+	int printed;
+	
 	char c = va_arg(arg, int);
+	printed = write_char(c, buffer,flags, width, precision,size);
+	return (printed);
+}
+
+/**
+ *
+ */
+
+int write_char(char c, char buffer[],int flags, int width, int precision, int size)
+{ /* char is stored at left and paddind at buffer's right */
+	int i = 0;
+	char padd = ' ';
+
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (flags & 4)
+		padd = '0';
+
 	buffer[i++] = c;
-	return (write(1,&buffer[0],1);
+	buffer[i] = '\0';
+
+	if (width > 1)
+	{
+		buffer[BUFSIZE - 1] = '\0';
+		for (i = 0; i < width - 1; i++)
+			buffer[BUFSIZE - i - 2] = padd;
+
+		if (flags & 1)
+			return (write(1, &buffer[0], 1) +
+					write(1, &buffer[BUFSIZE - i - 1], width - 1));
+		else
+			return (write(1, &buffer[BUFSIZE - i - 1], width - 1) +
+					write(1, &buffer[0], 1));
+	}
+
+	return (write(1, &buffer[0], 1));
+}
+
+
+/**
+ *
+ */
+
+int print_percent(va_list arg, char buffer[], int flags, int width, int precision, int size)
+{
+	UNUSED(arg);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return(write(1, %%, 1));
+}
+
+/**
+ *
+ */
+
+int print_str(va_list arg, char buffer[], int flags, int width, int precision, int size)
+{
+	int length, i;
+
+	char *str = va_arg(arg, char*);
+	UNUSED(buffer);
+        UNUSED(flags);
+        UNUSED(width);
+        UNUSED(precision);
+        UNUSED(size);
+
+	if (str == NULL)
+	{
+		str = "(null)";
+		if (precision >= 6)
+			str = "      ";
+	}
+
+	while (str[length] != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
+	{
+		if (flags & 1)
+		{
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - length; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &str[0], length);
+			return (width);
+		}
+	}
+
+	return (write(1, str, length));
 }
